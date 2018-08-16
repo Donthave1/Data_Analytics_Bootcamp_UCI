93ecda200f29bf608591cacce0ba52ddf246cc08
Edward Chen <eddychen0104@gmail.com>
2018-08-15 23:10:19 -0700

    sort function implementation to find winner

--- 02 Assignments/0818 Python - Moc Data Challenges/PyPoll/main.py
+++ 02 Assignments/0818 Python - Moc Data Challenges/PyPoll/main.py
@@ -2,24 +2,42 @@
 import os, csv
 
 #define resources path
-csv_path = os.path.join("..", "..", "..", "Resources", "election_data.csv")
+csv_path = os.path.join("..", "Resources", "election_data.csv")
 
-#make csv file an iteratable object
+#define variables
+total_votes = 0
+total_candidate = 0
+candidate_list = []
+candidate_vote = {}
+candidate_avg = {}
+
+#make csv file an iteratable object, including dictionary type of reader
 with open(csv_path, newline="", encoding="UTF-8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    #parse header
-    next(csv_reader)
-    #create candidate list and vote list
-    candidate_list = []
-    unique_candidate_list = []
-    total_vote = 0
-    vote_list = []
+    csv_reader = csv.DictReader(csv_file, delimiter=",")
+    #define dictionary of candidates:
     for row in csv_reader:
-        #find total votes
-        total_vote += 1
-        #compile a list of all candidate
-        candidate_list.append(row[2])
-        
-        
-    print(candidate_list)
-    print(total_vote)
+        #find total vote
+        total_votes += 1
+        #find total candidate
+        total_candidate = row["Candidate"]
+        #create candidate list; if candidate exist + 1 vote, 
+        #if candidate do not exist, append to the list
+        if row["Candidate"] not in candidate_list:
+            #add candidate to the list
+            candidate_list.append(row["Candidate"])
+            #new candidate earn first vote
+            candidate_vote[row["Candidate"]] = 1
+        else:
+            #existing candidate vote addition
+            candidate_vote[row["Candidate"]] += 1
+        #find candidate average
+        #candidate_avg[candidate_vote["Candidate"]] = round(candidate_vote["Candidate"] / total_votes * 100, 2)
+for candidate_name,vote_count in candidate_vote.items():
+    print(candidate_name + " " + str(vote_count) + " " + str(round(vote_count / total_votes * 100, 2)) + "%")
+
+#sort the candidate vote list
+sorted_candidates = [k for k in sorted(candidate_vote, key=candidate_vote.get, reverse=True)]
+print(sorted_candidates)
+#find the winner
+winner = sorted_candidates[0]
+print(winner)
