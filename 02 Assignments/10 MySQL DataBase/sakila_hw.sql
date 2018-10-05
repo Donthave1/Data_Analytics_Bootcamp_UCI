a0b51e8155f6fb656d30c773f79ca9b42c40de98
Edward Chen <eddychen0104@gmail.com>
2018-10-05 09:48:06 -0700

    SQL hw completed

--- 02 Assignments/10 MySQL DataBase/sakila_hw.sql
+++ 02 Assignments/10 MySQL DataBase/sakila_hw.sql
@@ -87,54 +87,150 @@
 -- );
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+-- SELECT first_name, last_name, address 
+-- FROM staff s
+-- JOIN address a 
+-- ON s.address_id = a.address_id;
 
-
--- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
-
+-- 6b. Use JOIN to display the total amount run up by each staff member in August of 2005. Use tables staff and payment.
+-- CREATE VIEW August_2005 AS
+-- SELECT staff_id, 
+-- SUM(amount)
+-- FROM payment 
+-- WHERE MONTH(payment_date) = 8 AND YEAR(payment_date) = 2005 
+-- GROUP BY staff_id;
+-- SELECT s.staff_id, s.first_name, s.last_name
+-- FROM staff s
+-- JOIN August_2005 AS a5 
+-- ON s.staff_id = a5.staff_id;
+-- DROP VIEW August_2005;
 
 -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
-
+-- SELECT title, COUNT(actor_id) AS Number_Of_Actors
+-- FROM film f
+-- INNER JOIN film_actor fa
+-- ON f.film_id = fa.film_id
+-- GROUP BY title;
 
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-
+-- SELECT COUNT(*) AS Counted_Copies 
+-- FROM inventory 
+-- WHERE film_id = (
+-- 	SELECT film_id 
+--     FROM film 
+--     WHERE title = 'Hunchback Impossible'
+-- );
 
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
-
+-- SELECT last_name, first_name, 
+-- SUM(amount)
+-- FROM payment p
+-- JOIN customer c
+-- ON p.customer_id = c.customer_id
+-- GROUP BY p.customer_id
+-- ORDER BY last_name ASC;
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
 -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
-
+-- SELECT title 
+-- FROM film
+-- WHERE language_id IN (
+-- 	SELECT language_id 
+-- 	FROM language
+-- 	WHERE name = 'English'
+--     ) AND (title LIKE 'K%' OR 'Q%');
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
-
+-- SELECT first_name, last_name
+-- FROM actor
+-- WHERE actor_id IN (
+-- 	SELECT actor_id 
+--     FROM film_actor
+-- 	WHERE film_id IN (
+-- 		SELECT film_id FROM film
+-- 		WHERE title = 'Alone Trip'
+--         )
+-- );
 
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
-
+-- SELECT country, last_name, first_name, email
+-- FROM country c
+-- JOIN customer cm
+-- ON c.country_id = cm.customer_id
+-- WHERE country = 'Canada';
 
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
-
+-- SELECT title, category
+-- FROM film_list
+-- WHERE category = 'Family';
 
 -- 7e. Display the most frequently rented movies in descending order.
-
+-- SELECT f.title, 
+-- COUNT(r.inventory_id) AS Rented_Counts
+-- FROM inventory i
+-- JOIN rental r
+-- ON i.inventory_id = r.inventory_id
+-- JOIN film f 
+-- ON i.film_id = f.film_id
+-- GROUP BY r.inventory_id
+-- ORDER BY COUNT(r.inventory_id) DESC;
 
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
-
+-- SELECT s.store_id, 
+-- SUM(amount)
+-- FROM store s
+-- JOIN staff sf
+-- ON s.store_id = sf.store_id
+-- JOIN payment p 
+-- ON p.staff_id = sf.staff_id
+-- GROUP BY s.store_id;
 
 -- 7g. Write a query to display for each store its store ID, city, and country.
+-- SELECT s.store_id, city, country
+-- FROM store s
+-- JOIN customer cm
+-- ON s.store_id = cm.store_id
+-- JOIN staff sf
+-- ON s.store_id = sf.store_id
+-- JOIN address a
+-- ON cm.address_id = a.address_id
+-- JOIN city ct
+-- ON a.city_id = ct.city_id
+-- JOIN country cnr
+-- ON ct.country_id = cnr.country_id;
 
 
 -- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
-
+-- SELECT c.name, 
+-- SUM(p.amount) AS 'Gross Revenue'
+-- FROM category c, film_category fc, inventory i, payment p, rental r
+-- WHERE c.category_id = fc.category_id
+-- AND fc.film_id = i.film_id
+-- AND i.inventory_id = r.inventory_id
+-- AND r.rental_id = p.rental_id
+-- GROUP BY c.name
+-- ORDER BY SUM(p.amount) DESC
+-- LIMIT 5;
 
 -- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. 
 -- If you haven't solved 7h, you can substitute another query to create a view.
-
+-- CREATE VIEW top_five_gg AS
+-- SELECT c.name, 
+-- SUM(p.amount) AS 'Gross Revenue'
+-- FROM category c, film_category fc, inventory i, payment p, rental r
+-- WHERE c.category_id = fc.category_id
+-- AND fc.film_id = i.film_id
+-- AND i.inventory_id = r.inventory_id
+-- AND r.rental_id = p.rental_id
+-- GROUP BY c.name
+-- ORDER BY SUM(p.amount) DESC
+-- LIMIT 5;
 
 -- 8b. How would you display the view that you created in 8a?
-
+-- SELECT * FROM top_five_gg;
 
 -- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
-
+-- DROP VIEW top_five_gg;
 
 
 
