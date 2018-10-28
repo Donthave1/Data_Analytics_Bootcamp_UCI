7b2c29154c0ff6c8d2f513cd2fdfe88836c43af2
Ed Chen <eddychen0104@gmail.com>
2018-10-28 16:07:22 -0700

    NASA Image scraped and Tweeter scraped

--- 02 Assignments/11 Destination Mars Live Feed/app.py
+++ 02 Assignments/11 Destination Mars Live Feed/app.py
@@ -10,33 +10,16 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def index():
-    mars_news = mongo.db.mars_news.find_one()
-    mars_pics = mongo.db.mars_pics.find_one()
-    return render_template("index.html", mars_news=mars_news, mars_pics=mars_pics, scraped_date=scraped_date)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars, scraped_date=scraped_date)
 
 # Scrape for News
-@app.route("/news")
-def news_scraper():
-    mars_news = mongo.db.mars_news
-    mars_news_data = scrape_mars.news_scrape()
-    mars_news.update({}, mars_news_data, upsert=True)
-
-# Scrape for Images
-@app.route("/images")
-def pics_scraper():
-    mars_pics = mongo.db.mars_pics
-    mars_pics_data = scrape_mars.pics_scrape()
-    mars_pics.update({}, mars_pics_data, upsert=True)
-
-# # Scrape for Weathers
-# mars_news = mongo.db.mars_news
-# mars_news_data = scrape_mars.scrape()
-# mars_news.update({}, mars_news_data, upsert=True)
-# # Scrape for Facts
-# mars_news = mongo.db.mars_news
-# mars_news_data = scrape_mars.scrape()
-# mars_news.update({}, mars_news_data, upsert=True)
-# return redirect("/", code=302)
+@app.route("/scrape")
+def scraper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 02 Assignments/11 Destination Mars Live Feed/scrape_mars.py
+++ 02 Assignments/11 Destination Mars Live Feed/scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import datetime as dt
+import time
 
 def init_browser():
 # @NOTE: Replace the path with your actual path to the chromedriver
@@ -8,52 +9,64 @@ def init_browser():
     return Browser("chrome", **executable_path, headless=True)
 
 # Function to scrape news
-def news_scrape():
+def scrape():
     browser = init_browser()
-    news = {}
+    data = {}
     news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(news_url)
     news_html = browser.html
     news_soup = bs(news_html, "lxml")
-    news["title"] = news_soup.find("div", class_="content_title").get_text()
-    return news
+    data["title"] = news_soup.find("div", class_="content_title").get_text()
+    data["content"] = news_soup.find("div", class_="article_teaser_body").get_text()
 
-# Function to scrape images
-def pics_scrape():
-    browser = init_browser()
-    pics = {}
     pics_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(pics_url)
     pics_html = browser.html
-    # create a soup object from the html
-    img_soup = bs(pics_html, "lxml")
-    elem = img_soup.find("div", class_="img")
-    featured_image_url = elem.find("img")["src"]
-    # add our src to surf data with a key of src
-    pics["src"] = featured_image_url
-    return pics
+    pics_soup = bs(pics_html, "lxml")
+    elem = pics_soup.find(class_="articles")
+    featured_image_url = "https://www.jpl.nasa.gov/" + elem.find("a")["data-fancybox-href"]
+    featured_thumbnail_url = "https://www.jpl.nasa.gov/" + elem.find("a")["data-thumbnail"]
+    data["pics"] = featured_image_url
+    data["thumbnail"] = featured_thumbnail_url
 
-# # Function to scrape images
-# def news_scrape():
-#     browser = init_browser()
-#     news = {}
-#     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-#     browser.visit(url)
-#     html = browser.html
-#     soup = bs(html, "lxml")
-#     news["title"] = soup.find("div", class_="content_title").get_text()
-#     return news
+    twt_url = "https://twitter.com/marswxreport?lang=en"
+    browser.visit(twt_url)
+    twt_html = browser.html
+    twt_soup = bs(twt_html, "lxml")
+    data["tweet"] = twt_soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").get_text()
 
-# # Function to scrape images
-# def news_scrape():
-#     browser = init_browser()
-#     news = {}
-#     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-#     browser.visit(url)
-#     html = browser.html
-#     soup = bs(html, "lxml")
-#     news["title"] = soup.find("div", class_="content_title").get_text()
-#     return news
+    # weather_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    # browser.visit(weather_url)
+    # weather_html = browser.html
+    # weather_soup = bs(weather_html, "lxml")
+    # hemi_list = weather_soup.find("div", id="product-section")
+    # pic_links = hemi_list.find("a")["href"]
+    # cerberus_link = "https://astrogeology.usgs.gov" + pic_links
+    # browser.visit(cerberus_link)
+    # cerb_html = browser.html
+    # cerb_soup = bs(cerb_html, "lxml")
+    # download_cerb = cerb_soup.find("div", class_="downloads")
+    # data["cerb"] = download_cerb.find("a")["href"]
+    # data["cerb_title"] = download_cerb.find("h2", class_="title").get_text()
+
+    weather_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(weather_url)
+
+    # Moving through pages
+    time.sleep(5)
+    browser.click_link_by_partial_text('Cerberus Hemisphere Enhanced')
+    time.sleep(5)
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    cerb_html = browser.html
+    cerb_soup = bs(cerb_html, "lxml")
+    download_cerb = cerb_soup.find("div", class_="downloads")
+    data["cerb"] = download_cerb.find("a")["href"]
+    data["cerb_title"] = download_cerb.find("h2", class_="title").get_text()
+
+
+    return data
+
 
 # Function to find today's date
 def today():
